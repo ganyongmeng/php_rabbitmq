75e1cad8509e2958f570866f11f34bdea0d2481b
Devin <ganym@500wan.com>
2021-03-25 11:17:10 +0800

    rabbitmq

--- demo/addtask.php
+++ demo/addtask.php
@@ -1,21 +1,9 @@
 <?php
-/**
- * consumer
- */
-require_once __DIR__ . '/../vendor/autoload.php';
-use ganyongmeng\php_rabbitmq\WebService\Consumer;
-$config = [
-    'RabbitMq' => [
-        // Rabbitmq 服务地址
-        'host' => '127.0.0.1',
-        // Rabbitmq 服务端口
-        'port' => '5672',
-        // Rabbitmq 帐号
-        'login' => 'guest',
-        // Rabbitmq 密码
-        'password' => 'guest',
-        'vhost'=>'/'
-    ]
-];
-$consumer = new Consumer($config, 'exchange_1', 'queue_1');
-$consumer->process();
+class Add
+{
+    public static function run($envelope, $queue) {
+        // Handle response
+        echo $envelope->getBody()."\n";
+    }
+
+}
